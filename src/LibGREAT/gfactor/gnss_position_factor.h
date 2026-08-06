78daff2156317c3a093d2696b612ac4559b2446f
whs <2318837909@qq.com>
2026-08-07 01:04:21 +0800

    新建位置因子头文件

---
+++ src/LibGREAT/gfactor/gnss_position_factor.h
@@ -0,0 +1,21 @@
+#ifndef GNSS_POSITION_FACTOR_H
+#define GNSS_POSITION_FACTOR_H
+/**
+ * @file         gimu_factor.h
+ * @author       GREAT-WHU (https://github.com/GREAT-WHU)
+ * @brief        Construction of imu factors for factor graph optimization.
+ * @version      1.0
+ * @date         2026-08
+ *
+ * @copyright Copyright (c) 2025, Wuhan University. All rights reserved.
+ *
+ */
+#include"gfgo/gutility.h"
+namespace gfgo
+{
+	class LibGREAT_LIBRARY_EXPORT GnssPositionFactor {
+
+
+	};
+}
+#endif
